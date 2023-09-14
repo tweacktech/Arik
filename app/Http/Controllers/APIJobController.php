dcd7f363b27bc908202d055ba8cc55f68faf6f06
YrayPixels <moseserhinyodavwe2@gmail.com>
2023-09-14 22:34:53 +0100

    sent

--- app/Http/Controllers/APIJobController.php
+++ app/Http/Controllers/APIJobController.php
@@ -8,8 +8,27 @@ class APIJobController extends Controller
 {
     public function login(Request $request)
     {
-
         $email = $request->email;
         $password = $request->password;
+    }
+
+    public function joblist()
+    {
+    }
+
+    public function jobdetails($id)
+    {
+    }
+
+    public function jobschedules($id)
+    {
+    }
+
+    public function myapplications($id)
+    {
+    }
+
+    public addtosavedjobs(Request $request){
+
     }
 }
